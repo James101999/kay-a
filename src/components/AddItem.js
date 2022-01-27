b1b34fed62124ca3ff0ef9bd17cb64f6624a3e2c
chraem <calumbiranc@gmail.com>
2022-01-27 21:19:30 +0800

    [fix] removing items when quantity is 0, [feat] save list to local storage

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -11,18 +11,38 @@ export default function AddItem() {
     // AddItem Handler
     const addItem = (event) => {
         event.preventDefault();
-        // console.log(event.target);
+        var i = 0;
         const formData = event.target;
-        const newItem = {
-            item_name: formData.item_name.value,
-            expiration_date: formData.expiration_date.value,
-            quantity: Number(formData.quantity.value)
-        };
-        // console.log("newItem", newItem);
+        const newItem = {};
+        const length = items.length
 
-        // Add item inside items[]
+        if (length == 0) {
+            newItem['item_name'] = formData.item_name.value;
+            newItem['expiration_date'] = formData.expiration_date.value;
+            newItem['quantity'] = Number(formData.quantity.value);
+
+        } else {
+            for (i = 0; i < length; i++) {
+                let existingName = items[i]['item_name'];
+                console.log(existingName);
+
+                if (formData.item_name.value == existingName) {
+                    newItem['item_name'] = formData.item_name.value + "(" + length.toString() + ")";
+                    newItem['expiration_date'] = formData.expiration_date.value;
+                    newItem['quantity'] = Number(formData.quantity.value);
+                    break;
+                }
+                else {
+                    newItem['item_name'] = formData.item_name.value;
+                    newItem['expiration_date'] = formData.expiration_date.value;
+                    newItem['quantity'] = Number(formData.quantity.value);
+                }
+
+            }
+
+        }
         setItem([...items, newItem]);
-        console.log("items", items);
+
     };
 
     const incrementQuantity = (event) => {
@@ -38,9 +58,16 @@ export default function AddItem() {
         const itemIndex = event.target.value;
         items[itemIndex].quantity = items[itemIndex].quantity - 1;
         setItem([...items])
-        // console.log(items)
+        if (items[itemIndex].quantity < 0) {
+            setItem(items.filter(item => item.item_name !== items[itemIndex].item_name))
+            delete items[items[itemIndex].item_name]
+        }
+        // console.log(items);
     };
 
+    const saveToLocalStorage = (event) => {
+        console.log(localStorage.setItem("items_LocalStorage", JSON.stringify(items)));
+    }
 
     return (
         <div>
@@ -58,11 +85,14 @@ export default function AddItem() {
 
                     <Form.Group className="mb-3" controlId="formBasicQuantity">
                         <Form.Label>Quantity:</Form.Label>
-                        <Form.Control type="text" placeholder="Enter item's quantity" name="quantity" />
+                        <Form.Control type="number" placeholder="Enter item's quantity" name="quantity" />
                     </Form.Group>
 
                     <Button variant="primary" type="submit">
                         Add
+                    </Button>
+                    <Button variant="primary" onClick={saveToLocalStorage}>
+                        Save
                     </Button>
                 </Form>
             </div>
@@ -91,7 +121,7 @@ export default function AddItem() {
                                         <td> {item.quantity} </td>
                                         <td>
                                             <Button variant="success" onClick={event => incrementQuantity(event)} value={index}>Add</Button>
-                                            <Button variant="danger" onClick={event => decrementQuantity(event)} value={index}>Minus</Button>
+                                            <Button variant="danger" onClick={event => decrementQuantity(event)} value={index} >Minus</Button>
                                         </td>
                                     </tr>
                                 )
